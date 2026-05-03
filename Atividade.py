09eade56d22055697996a0512313c511753b6c3e
ludovicoluka <lucasollua@gmail.com>
2026-05-02 21:13:09 -0300

    Add files via upload

---
+++ Atividade.py
@@ -0,0 +1,10 @@
+print('Python na Escola de Programação da Alura')
+
+nome = 'Lucas'
+idade = '27'
+print('Meu nome é', nome, 'e tenho', idade, 'anos.')
+
+print('A','L','U','R','A',sep='\n')
+
+pi = 3.14159
+print(f'O valor arredondado de pi é: {pi:.2f}')
